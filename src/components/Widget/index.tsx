bc68862a11734ef4f9a86f0485faba7dc4611196
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-05-06 20:35:34 -0300

    Passando as props pro componente Success

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { TouchableOpacity } from 'react-native';
 import { ChatTeardropDots } from 'phosphor-react-native';
 import { styles } from './styles';
@@ -14,9 +14,20 @@ export type FeedbackType = keyof typeof feedbackTypes;
 
 function Widget() {
   const bottomSheetRef = useRef<BottomSheet>(null);
+  const [ feedbackType, setFeedbackType ] = useState<FeedbackType | null>(null);
+  const [ feedbackSent, setFeedbackSent ] = useState(false);
 
   function handleOpen() {
     bottomSheetRef.current?.expand();
+  }
+
+  function handleRestartFeedback() {
+    setFeedbackType(null);
+    setFeedbackSent(false);
+  }
+
+  function handleFeedbackSent() {
+    setFeedbackSent(true);
   }
 
   return(
@@ -37,9 +48,26 @@ function Widget() {
         backgroundStyle={styles.modal}
         handleIndicatorStyle={styles.indicator}
       >
-        <Form 
-          feedbackType="BUG"
-        />
+        {
+          feedbackSent 
+          ? 
+          <Success 
+            onSendingAnotherFeedback={handleRestartFeedback} 
+          /> 
+          : 
+          <>
+            {
+              feedbackType ? 
+              <Form 
+                feedbackType={feedbackType}
+                onFeedbackCanceled={handleRestartFeedback}
+                onFeedbackSent={handleFeedbackSent} 
+              />
+              :
+              <Options onFeedbackTypeChanged={setFeedbackType} />
+            }
+          </>
+        }
 
       </BottomSheet>
     </>
